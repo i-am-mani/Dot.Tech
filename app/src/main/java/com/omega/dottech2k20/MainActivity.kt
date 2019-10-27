15d5ea3b5acd80c975885126e4c003ae845bfc1b
Mandeep <mandeepch868@gmail.com>
2019-10-27 16:49:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/omega/dottech2k20/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.omega.dottech2k20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
