3e4cd521c44f4c284cb2b32a6a451bda14e2a8b1
Mandeep <mandeepch868@gmail.com>
2019-10-28 21:22:41 +0530

    Feature: bottom navigation view

--- app/src/main/java/com/omega/dottech2k20/MainActivity.kt
+++ app/src/main/java/com/omega/dottech2k20/MainActivity.kt
@@ -2,11 +2,23 @@ package com.omega.dottech2k20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+
+        navigation_bar.add(MeowBottomNavigation.Model(1, R.drawable.ic_profile_user))
+        navigation_bar.add(MeowBottomNavigation.Model(2, R.drawable.ic_events))
+        navigation_bar.add(MeowBottomNavigation.Model(3, R.drawable.ic_notification_bell))
+
+        navigation_bar.show(2,true)
     }
 }
