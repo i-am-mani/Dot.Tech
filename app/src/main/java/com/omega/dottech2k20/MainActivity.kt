005d8df592bec3194947e445f3d92c50607b01c4
Mandeep <mandeepch868@gmail.com>
2019-10-31 15:52:04 +0530

    Feature: Navigation View

--- app/src/main/java/com/omega/dottech2k20/MainActivity.kt
+++ app/src/main/java/com/omega/dottech2k20/MainActivity.kt
@@ -1,18 +1,54 @@
 package com.omega.dottech2k20
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initBottomNavigationView()
+        setSupportActionBar(toolbar)
+        window.statusBarColor = Color.TRANSPARENT
+
+//        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+//        supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+
+        val navController = nav_host_fragment.findNavController()
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawer_layout)
+
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+
     }
 
+    private fun setupNavigationMenu(navController: NavController) {
+        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
+        sideNavView?.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(nav_host_fragment.findNavController(),drawer_layout)
+                || super.onSupportNavigateUp()
+    }
     private fun initBottomNavigationView() {
 
         navigation_bar.add(MeowBottomNavigation.Model(1, R.drawable.ic_profile_user))
