d049d3b1ab001b3b9e20aeccd0eb537489b3e686
Mandeep <mandeepch868@gmail.com>
2019-11-01 10:57:39 +0530

    Hide bottom nav bar when on fragments not on nav bar

--- app/src/main/java/com/omega/dottech2k20/MainActivity.kt
+++ app/src/main/java/com/omega/dottech2k20/MainActivity.kt
@@ -2,6 +2,7 @@ package com.omega.dottech2k20
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
@@ -19,6 +20,7 @@ import kotlinx.android.synthetic.main.content_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +33,20 @@ class MainActivity : AppCompatActivity() {
 //        supportActionBar!!.setDisplayShowHomeEnabled(true)
 
 
-        val navController = nav_host_fragment.findNavController()
+        navController = nav_host_fragment.findNavController()
         appBarConfiguration = AppBarConfiguration(navController.graph,drawer_layout)
 
         NavigationUI.setupWithNavController(nav_view, navController)
-        NavigationUI.setupActionBarWithNavController(this, navController)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
-
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        navController.addOnDestinationChangedListener{controller, destination, arguments ->
+            run {
+                if (destination.id != R.id.eventsFragment) {
+                    navigation_bar.visibility = View.INVISIBLE
+                } else{
+                    navigation_bar.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     private fun setupNavigationMenu(navController: NavController) {
@@ -50,11 +59,24 @@ class MainActivity : AppCompatActivity() {
                 || super.onSupportNavigateUp()
     }
     private fun initBottomNavigationView() {
-
         navigation_bar.add(MeowBottomNavigation.Model(1, R.drawable.ic_profile_user))
         navigation_bar.add(MeowBottomNavigation.Model(2, R.drawable.ic_events))
         navigation_bar.add(MeowBottomNavigation.Model(3, R.drawable.ic_notification_bell))
 
         navigation_bar.show(2,true)
+        navigation_bar.setOnClickMenuListener {
+            when(it.id){
+                1 -> {
+                    navController.navigate(R.id.eventsFragment)
+                }
+
+                2 -> {
+                  // todo Add navigation to profile
+                }
+                3 -> {
+                    // todo Add navigation to Notification
+                }
+            }
+        }
     }
 }
