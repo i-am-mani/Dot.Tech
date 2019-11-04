5e14a99453267cd5ac58b44118e5e262bf58ddc4
Mandeep <mandeepch868@gmail.com>
2019-11-04 13:13:24 +0530

    Remove @Exclude on id property

--- app/src/main/java/com/omega/dottech2k20/Models/Event.kt
+++ app/src/main/java/com/omega/dottech2k20/Models/Event.kt
@@ -1,16 +1,15 @@
 package com.omega.dottech2k20.Models
 
 import com.google.firebase.Timestamp
-import com.google.firebase.firestore.Exclude
 import com.google.firebase.firestore.ServerTimestamp
 
 data class Event(
-    @Exclude var id: String? = null,
-    var title: String? = null,
-    var thumbNail: String? = null,
-    var shortDescription: String? = null,
-    var participantCount: Int? = null,
-    @ServerTimestamp val startTime: Timestamp? = null,
-    @ServerTimestamp val endTime: Timestamp? = null
+	var id: String? = null,
+	var title: String? = null,
+	var thumbnail: String? = null,
+	var shortDescription: String? = null,
+	var participantCount: Int? = null,
+	@ServerTimestamp val startTime: Timestamp? = null,
+	@ServerTimestamp val endTime: Timestamp? = null
 )
 
