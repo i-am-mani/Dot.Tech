e503b633baae5b99856397f613f0d4702074a15f
Mandeep <mandeepch868@gmail.com>
2019-11-14 16:17:26 +0530

    Change var to val

--- app/src/main/java/com/omega/dottech2k20/Models/Event.kt
+++ app/src/main/java/com/omega/dottech2k20/Models/Event.kt
@@ -4,11 +4,11 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ServerTimestamp
 
 data class Event(
-	var id: String? = null,
-	var title: String? = null,
-	var thumbnail: String? = null,
-	var shortDescription: String? = null,
-	var participantCount: Int? = null,
+	val id: String? = null,
+	val title: String? = null,
+	val thumbnail: String? = null,
+	val shortDescription: String? = null,
+	val participantCount: Int? = null,
 	@ServerTimestamp val startTime: Timestamp? = null,
 	@ServerTimestamp val endTime: Timestamp? = null
 )
