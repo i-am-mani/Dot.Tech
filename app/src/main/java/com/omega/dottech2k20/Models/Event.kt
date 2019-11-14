dbee2cedaccb42df091e3b173e6679f6a0c6309c
Mandeep <mandeepch868@gmail.com>
2019-11-14 17:20:26 +0530

    Update images type: List<String>?, and implement Parcelable

--- app/src/main/java/com/omega/dottech2k20/Models/Event.kt
+++ app/src/main/java/com/omega/dottech2k20/Models/Event.kt
@@ -1,5 +1,7 @@
 package com.omega.dottech2k20.Models
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ServerTimestamp
 
@@ -10,8 +12,41 @@ data class Event(
 	val shortDescription: String? = null,
 	val participantCount: Int? = null,
 	val longDescription: String? = null,
-	val images: String? = null,
+	val images: List<String>? = null,
 	@ServerTimestamp val startTime: Timestamp? = null,
 	@ServerTimestamp val endTime: Timestamp? = null
-)
+) : Parcelable {
+	constructor(source: Parcel) : this(
+		source.readString(),
+		source.readString(),
+		source.readString(),
+		source.readString(),
+		source.readValue(Int::class.java.classLoader) as Int?,
+		source.readString(),
+		source.createStringArrayList(),
+		source.readParcelable<Timestamp>(Timestamp::class.java.classLoader),
+		source.readParcelable<Timestamp>(Timestamp::class.java.classLoader)
+	)
 
+	override fun describeContents() = 0
+
+	override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+		writeString(id)
+		writeString(title)
+		writeString(thumbnail)
+		writeString(shortDescription)
+		writeValue(participantCount)
+		writeString(longDescription)
+		writeStringList(images)
+		writeParcelable(startTime, 0)
+		writeParcelable(endTime, 0)
+	}
+
+	companion object {
+		@JvmField
+		val CREATOR: Parcelable.Creator<Event> = object : Parcelable.Creator<Event> {
+			override fun createFromParcel(source: Parcel): Event = Event(source)
+			override fun newArray(size: Int): Array<Event?> = arrayOfNulls(size)
+		}
+	}
+}
