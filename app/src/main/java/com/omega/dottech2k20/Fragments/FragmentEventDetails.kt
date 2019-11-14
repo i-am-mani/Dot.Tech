14653bc7b9c1f62a8c71c48c2e402df855c100a6
Mandeep <mandeepch868@gmail.com>
2019-11-14 17:19:10 +0530

    Implement RV images and event details

--- app/src/main/java/com/omega/dottech2k20/Fragments/FragmentEventDetails.kt
+++ app/src/main/java/com/omega/dottech2k20/Fragments/FragmentEventDetails.kt
@@ -6,18 +6,57 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.omega.dottech2k20.Adapters.EventImageItem
+import com.omega.dottech2k20.Models.Event
 import com.omega.dottech2k20.R
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.fragment_event_details.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class FragmentEventDetails : Fragment() {
+
+	lateinit var mEvent: Event
+	val mAdapter: GroupAdapter<GroupieViewHolder> = GroupAdapter()
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
+		mEvent = savedInstanceState?.getParcelable("event")!!
 		return inflater.inflate(R.layout.fragment_event_details, container, false)
 	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		initRV()
+		initEventDetails()
+	}
+
+	private fun initEventDetails() {
+		tv_event_title.text = mEvent.title
+		tv_event_details.text = mEvent.longDescription
+	}
+
+	private fun initRV() {
+		generateItems()
+
+		rv_event_images.adapter = mAdapter
+		rv_event_images.layoutManager =
+			LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+	}
+
+	private fun generateItems() {
+		val items = mutableListOf<EventImageItem>()
+
+		mEvent.images?.let { images ->
+			for (image in images) {
+				items.add(EventImageItem(image))
+			}
+		}
+
+		mAdapter.addAll(items)
+	}
+
 
 }
