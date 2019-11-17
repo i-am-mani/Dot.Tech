1db0e5b0b8b6b872864c0f773b312dc629b8d096
Mandeep <mandeepch868@gmail.com>
2019-11-17 08:36:14 +0530

    Implement Join/Leave and add Danger color

--- app/src/main/java/com/omega/dottech2k20/Fragments/FragmentEventDetails.kt
+++ app/src/main/java/com/omega/dottech2k20/Fragments/FragmentEventDetails.kt
@@ -1,6 +1,7 @@
 package com.omega.dottech2k20.Fragments
 
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Html
 import android.transition.ChangeBounds
@@ -10,11 +11,18 @@ import android.view.*
 import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import com.omega.dottech2k20.Adapters.EventImageItem
+import com.omega.dottech2k20.MainActivity
 import com.omega.dottech2k20.Models.Event
+import com.omega.dottech2k20.Models.UserEventViewModel
 import com.omega.dottech2k20.R
+import com.omega.dottech2k20.Utils.AuthenticationUtils
+import com.omega.dottech2k20.Utils.BinaryDialog
 import com.omega.dottech2k20.Utils.HorizontalEqualSpaceItemDecoration
 import com.omega.dottech2k20.Utils.Utils
 import com.xwray.groupie.GroupAdapter
@@ -25,8 +33,37 @@ class FragmentEventDetails : Fragment() {
 
 	private val TAG: String = javaClass.simpleName
 	lateinit var mEvent: Event
-	val mAdapter: GroupAdapter<GroupieViewHolder> = GroupAdapter()
+	private val mAdapter: GroupAdapter<GroupieViewHolder> = GroupAdapter()
 	val SWIPE_THRESHOLD = 2000
+	lateinit var mActivity: MainActivity
+	lateinit var mViewModel: UserEventViewModel
+
+	override fun onAttach(context: Context) {
+		super.onAttach(context)
+		mActivity = context as MainActivity
+	}
+
+	override fun onActivityCreated(savedInstanceState: Bundle?) {
+		super.onActivityCreated(savedInstanceState)
+		mViewModel = ViewModelProviders.of(mActivity).get(UserEventViewModel::class.java)
+		mViewModel.getUserEvent()?.observe(this, Observer {
+			it?.let {
+				val event = it.find { e -> e.id == mEvent.id }
+				updateButton(event)
+			}
+		})
+	}
+
+	private fun updateButton(event: Event?) {
+		if (event == null) {
+			btn_join.show()
+			btn_leave.hide()
+		} else {
+			btn_leave.show()
+			btn_join.hide()
+		}
+	}
+
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
@@ -39,9 +76,77 @@ class FragmentEventDetails : Fragment() {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+		hideButtons()
 		initRV()
 		initEventDetails()
 		attachFlingListener()
+		addClickListeners()
+	}
+
+	private fun hideButtons() {
+		btn_leave.hide()
+		btn_join.hide()
+	}
+
+	private fun addClickListeners() {
+		setJoinEventCallback()
+		setLeaveEventCallback()
+		btn_leave.hide()
+	}
+
+	private fun setJoinEventCallback() {
+		btn_join.setOnClickListener {
+			if (AuthenticationUtils.currentUser == null) {
+				requestForLoginDialog()
+			} else {
+				context?.let { c ->
+					BinaryDialog(c, R.layout.dialog_event_confirmation).apply {
+						title = "Join This Event ?"
+						rightButtonCallback = {
+							Log.d(TAG, "btn_join triggered")
+							mViewModel.joinEvent(mEvent)
+						}
+						leftButtonCallback = { }
+					}.build()
+				}
+			}
+		}
+	}
+
+	private fun navigateToEvents() {
+		findNavController().navigate(R.id.eventsFragment)
+	}
+
+	private fun setLeaveEventCallback() {
+		btn_leave.setOnClickListener {
+
+			context?.let { c ->
+				BinaryDialog(c, R.layout.dialog_event_confirmation).apply {
+					title = "Leave this Event ?"
+					rightButtonCallback = {
+						mViewModel.unjoinEvents(mEvent)
+					}
+					leftButtonCallback = { }
+				}.build()
+			}
+
+
+		}
+	}
+
+	private fun requestForLoginDialog() {
+		context?.let { c ->
+			BinaryDialog(c).apply {
+				title = "Unsigned user"
+				description = "Please Sign in or Sign Up to Continue"
+				leftButtonName = "Sign In"
+				rightButtonName = "Sign Up"
+				rightButtonId = R.id.btn_right
+				leftButtonId = R.id.btn_left
+				leftButtonCallback = { findNavController().navigate(R.id.signInFragment) }
+				rightButtonCallback = { findNavController().navigate(R.id.signUpFragment) }
+			}.build()
+		}
 	}
 
 	private fun attachFlingListener() {
@@ -53,6 +158,11 @@ class FragmentEventDetails : Fragment() {
 	}
 
 	inner class FlingGestureListener : GestureDetector.SimpleOnGestureListener() {
+
+		/**
+		 * On Bottom-Top Fling use alternative layout - excluding images
+		 * On Top-Bottom Fling use normal layout with images
+		 */
 		override fun onFling(
 			e1: MotionEvent?,
 			e2: MotionEvent?,
@@ -64,8 +174,8 @@ class FragmentEventDetails : Fragment() {
 				Log.d(TAG, "Swipe Detected!")
 				val layout = R.layout.fragment_event_details
 				val animDuration = 500L
-
 				applyConstraints(layout, animDuration)
+
 			} else if (velocityY < -SWIPE_THRESHOLD) {
 				val layout = R.layout.fragment_event_details_maximized
 				val animDuration = 500L
@@ -85,7 +195,6 @@ class FragmentEventDetails : Fragment() {
 				duration = animDuration
 				interpolator = AccelerateDecelerateInterpolator()
 			}
-
 			set.applyTo(root_event_details)
 			TransitionManager.beginDelayedTransition(root_event_details, changeBounds)
 		}
@@ -105,15 +214,9 @@ class FragmentEventDetails : Fragment() {
 		context?.let {
 			LinearSnapHelper().attachToRecyclerView(rv_event_images)
 			rv_event_images.addItemDecoration(
-				HorizontalEqualSpaceItemDecoration(
-					Utils.convertDPtoPX(
-						it,
-						10
-					)
-				)
+				HorizontalEqualSpaceItemDecoration(Utils.convertDPtoPX(it, 10))
 			)
 		}
-
 	}
 
 	private fun generateItems() {
