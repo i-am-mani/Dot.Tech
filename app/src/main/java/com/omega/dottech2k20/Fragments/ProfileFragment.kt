4c0908af68c41f8c1521d2be770e253018704baf
Mandeep <mandeepch868@gmail.com>
2019-11-19 19:33:39 +0530

    Implement view details callback for user event item

--- app/src/main/java/com/omega/dottech2k20/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/omega/dottech2k20/Fragments/ProfileFragment.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -83,7 +84,7 @@ class ProfileFragment : Fragment() {
 	}
 
 	private fun viewDetails(events: Event) {
-		//todo Implement View Details
+		findNavController().navigate(R.id.event_details, bundleOf("event" to events))
 	}
 
 	private fun updateProfileDetails(user: User) {
