f3341a538e8b6c3e7672544172ecd770a2a0cdd2
Mandeep <mandeepch868@gmail.com>
2019-12-26 16:51:48 +0530

    Implement Page indicator

--- app/src/main/java/com/omega/dottech2k20/Fragments/SponsorsFragment.kt
+++ app/src/main/java/com/omega/dottech2k20/Fragments/SponsorsFragment.kt
@@ -2,6 +2,7 @@ package com.omega.dottech2k20.Fragments
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,18 +10,24 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.omega.dottech2k20.Adapters.SponsorItem
 import com.omega.dottech2k20.Models.Sponsor
 import com.omega.dottech2k20.Models.SponsorsViewModel
 import com.omega.dottech2k20.R
+import com.rd.animation.type.AnimationType
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import kotlinx.android.synthetic.main.fragment_sponsors.*
 
 class SponsorsFragment : Fragment() {
 
+	val TAG = "SponsorsFragment"
 	val mAdapter: GroupAdapter<GroupieViewHolder> = GroupAdapter()
 	lateinit var mViewModel: SponsorsViewModel
+	val linearLayoutManager =
+		LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +42,7 @@ class SponsorsFragment : Fragment() {
 				} else {
 					mAdapter.addAll(sponsorItems)
 				}
+				pageIndicatorView.count = mAdapter.itemCount // specify total count of indicators
 			}
 		})
 		return inflater.inflate(R.layout.fragment_sponsors, container, false)
@@ -50,9 +58,32 @@ class SponsorsFragment : Fragment() {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+
 		rv_sponsors.layoutManager =
-			LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+			linearLayoutManager
 		rv_sponsors.adapter = mAdapter
 		rv_sponsors.isNestedScrollingEnabled = false
+		LinearSnapHelper().attachToRecyclerView(rv_sponsors)
+
+		pageIndicatorView.count = 1
+		pageIndicatorView.selection = 0
+		pageIndicatorView.setAnimationType(AnimationType.DROP)
+		pageIndicatorView.radius = 12
+
+		rv_sponsors.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+			var position = 0
+
+			override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+				super.onScrollStateChanged(recyclerView, newState)
+				if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+					var curPosition = linearLayoutManager.findFirstCompletelyVisibleItemPosition()
+					Log.d(TAG, "curPosition = $curPosition")
+					if (curPosition != position) {
+						pageIndicatorView.selection = curPosition
+						position = curPosition
+					}
+				}
+			}
+		})
 	}
 }
