779e3204c9f436e60aaef1332da9f8a7556be9d7
Mandeep <mandeepch868@gmail.com>
2019-12-30 14:59:45 +0530

    implement requestFaq method to write query to MetaData collection

--- app/src/main/java/com/omega/dottech2k20/Models/MetaDataViewModel.kt
+++ app/src/main/java/com/omega/dottech2k20/Models/MetaDataViewModel.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MetaDataViewModel(application: Application) : AndroidViewModel(application) {
@@ -17,7 +19,6 @@ class MetaDataViewModel(application: Application) : AndroidViewModel(application
 	val META_DATA_COLLECTION_NAME = "MetaData"
 
 	fun getFAQs(): LiveData<List<FAQ>> {
-
 		if (!::mFaqLiveData.isInitialized) {
 			mFaqLiveData = MutableLiveData()
 			val query = mFirestore.collection(META_DATA_COLLECTION_NAME).document(FAQ_DOC_NAME)
@@ -27,14 +28,13 @@ class MetaDataViewModel(application: Application) : AndroidViewModel(application
 					result?.let { res ->
 						val data = res.data
 						if (data != null) {
-							Log.d(TAG, "Successfully fetched FAQs - ${data["faqs"]}")
+
 							val faqs = data["faqs"] as List<HashMap<String, String>>
 							val listOfFaqs = mutableListOf<FAQ>()
 							for (faq in faqs) {
 								val question = faq["question"]
 								val answer = faq["answer"]
 								if (question != null && answer != null) {
-
 									listOfFaqs.add(FAQ(question, answer))
 								}
 							}
@@ -44,7 +44,19 @@ class MetaDataViewModel(application: Application) : AndroidViewModel(application
 				}
 			}
 		}
-
 		return mFaqLiveData
+	}
+
+	fun requestQuery(uid: String, query: String) {
+		val queryMap = hashMapOf<String, String>()
+		queryMap[uid] = query
+
+		val firestoreQuery = mFirestore.collection("MetaData").document(FAQ_DOC_NAME)
+		firestoreQuery.update(FieldPath.of("pendingFaqs"), FieldValue.arrayUnion(queryMap))
+			.addOnCompleteListener {
+				if (it.isSuccessful) {
+					Log.i(TAG, "FAQ request successfully placed : $queryMap")
+				}
+			}
 	}
 }
