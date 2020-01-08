c8604d105ff9c6621cef489d610c74280570edb1
Mandeep <mandeepch868@gmail.com>
2020-01-08 18:57:31 +0530

    Support different backoff time with default as 10M

--- app/src/main/java/com/omega/dottech2k20/dialogs/BackOffDialog.kt
+++ app/src/main/java/com/omega/dottech2k20/dialogs/BackOffDialog.kt
@@ -4,16 +4,38 @@ import android.content.Context
 
 object BackOffDialog {
 
-	fun show(context: Context, time: Long) {
+	/**
+	 * Show BackOffDialog with Cool down time remaining.
+	 *
+	 * By defualt backoffTime is set to 10M
+	 */
+	fun show(context: Context, cooldownTime: Long, backOfTime: Long = 10) {
 		BinaryDialog(context).apply {
 			title = "Try again later \uD83D\uDE35"
+
+			val backOffTimeString = getTimeString(backOfTime)
+			val cooldownTimeString = getTimeString(cooldownTime)
 			description =
-				"It looks like you have joined and left the event in last 10 minutes.\n" +
-						"Please wait and try again after ${time}M"
+				"It looks like you have joined and left the event in last $backOffTimeString.\n" +
+						"Please wait and try again after ${cooldownTimeString}M"
 			isLeftButtonVisible = false
 			rightButtonName = "Close"
 			leftButtonCallback = {}
 			rightButtonCallback = {}
 		}.build()
+	}
+
+	private fun getTimeString(time: Long): String {
+		val hours = time / 60
+		val minutes = time % 60
+		var builder = ""
+
+		if (hours != 0L) {
+			builder += "$hours H "
+		}
+		if (minutes != 0L) {
+			builder += "$minutes M"
+		}
+		return builder.trim()
 	}
 }
