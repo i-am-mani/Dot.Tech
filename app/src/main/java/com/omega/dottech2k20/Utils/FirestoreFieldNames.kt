69ee6e727637819f48dc0eeb1af908ddb0594acd
Mandeep <mandeepch868@gmail.com>
2020-01-08 20:19:20 +0530

    Add Event and User Collection fields

--- app/src/main/java/com/omega/dottech2k20/Utils/FirestoreFieldNames.kt
+++ app/src/main/java/com/omega/dottech2k20/Utils/FirestoreFieldNames.kt
@@ -5,6 +5,10 @@ package com.omega.dottech2k20.Utils
  */
 object FirestoreFieldNames {
 
+	val EVENT_PARTICIPANTS_COUNT_FIELD: String = "participantCount"
+	val USERS_EVENT_FIELD: String = "events"
+	val TEAMMATES_FIELD: String = "teammates"
+	val USER_COLLECTION: String = "Users"
 	val EVENT_TYPE_INDIVIDUAL = "individual"
 	val EVENT_TYPE_TEAM = "team"
 	val EVENT_COLLECTION = "Events"
