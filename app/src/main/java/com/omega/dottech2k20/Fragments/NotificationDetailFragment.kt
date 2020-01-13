27263799fe46a8a9790ddbcb6bef5335f2638c01
Mandeep <mandeepch868@gmail.com>
2020-01-13 14:49:20 +0530

    Update date visibility

--- app/src/main/java/com/omega/dottech2k20/Fragments/NotificationDetailFragment.kt
+++ app/src/main/java/com/omega/dottech2k20/Fragments/NotificationDetailFragment.kt
@@ -3,6 +3,7 @@ package com.omega.dottech2k20.Fragments
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
@@ -48,8 +49,10 @@ class NotificationDetailFragment : Fragment() {
 	}
 
 	private fun setDate(issuedTime: Timestamp?) {
-		issuedTime?.let {
+		if (issuedTime != null) {
 			tv_notification_date.text = Utils.getFormattedTime(issuedTime)
+		} else {
+			tv_notification_date.visibility = GONE
 		}
 	}
 
