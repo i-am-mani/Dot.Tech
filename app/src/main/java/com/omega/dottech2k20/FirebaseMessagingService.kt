ffb1f421c3340432abedd55e1e69902a25cd7f89
Mandeep <mandeepch868@gmail.com>
2020-01-15 21:15:16 +0530

    Implement background notification

--- app/src/main/java/com/omega/dottech2k20/FirebaseMessagingService.kt
+++ app/src/main/java/com/omega/dottech2k20/FirebaseMessagingService.kt
@@ -1,6 +1,15 @@
 package com.omega.dottech2k20
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -11,9 +20,18 @@ class MessagingService : FirebaseMessagingService() {
 		Log.d(TAG, "Refreshed token: $token")
 	}
 
-	override fun onMessageReceived(p0: RemoteMessage) {
-		super.onMessageReceived(p0)
-		Log.d(TAG, "New Message Received!")
+	override fun onMessageReceived(message: RemoteMessage) {
+		super.onMessageReceived(message)
+		val nMessage = message.notification
+		if (nMessage != null) {
+			val title = nMessage.title
+			val message = nMessage.body
+			val imageUrl = nMessage.imageUrl
+			if (title != null && message != null) {
+				sendNotification(title, message, imageUrl)
+			}
+			Log.d(TAG, "Remote Message Received, data = $title  from = $message")
+		}
 	}
 
 //
@@ -66,34 +84,39 @@ class MessagingService : FirebaseMessagingService() {
 //	 *
 //	 * @param messageBody FCM message body received.
 //	 */
-//	private fun sendNotification(messageBody: String) {
-//		val intent = Intent(this, MainActivity::class.java)
-//		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//		val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-//			PendingIntent.FLAG_ONE_SHOT)
-//
-//		val channelId = getString(R.string.default_notification_channel_id)
-//		val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-//		val notificationBuilder = NotificationCompat.Builder(this, channelId)
-//			.setSmallIcon(R.drawable.ic_stat_ic_notification)
-//			.setContentTitle(getString(R.string.fcm_message))
-//			.setContentText(messageBody)
-//			.setAutoCancel(true)
-//			.setSound(defaultSoundUri)
-//			.setContentIntent(pendingIntent)
-//
-//		val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//
-//		// Since android Oreo notification channel is needed.
-//		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//			val channel = NotificationChannel(channelId,
-//				"Channel human readable title",
-//				NotificationManager.IMPORTANCE_DEFAULT)
-//			notificationManager.createNotificationChannel(channel)
-//		}
-//
-//		notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
-//	}
+private fun sendNotification(title: String, message: String, imageUrl: Uri?) {
+	val intent = Intent(this, MainActivity::class.java)
+	intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+	val pendingIntent = PendingIntent.getActivity(
+		this, 0 /* Request code */, intent,
+		PendingIntent.FLAG_ONE_SHOT
+	)
+
+	val channelId = getString(R.string.channel_id)
+	val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+	val notificationBuilder = NotificationCompat.Builder(this, channelId)
+		.setSmallIcon(R.drawable.ic_notification_bell)
+		.setContentTitle(title)
+		.setContentText(message)
+		.setAutoCancel(true)
+		.setSound(defaultSoundUri)
+		.setContentIntent(pendingIntent)
+
+
+	val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+	// Since android Oreo notification channel is needed.
+	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+		val channel = NotificationChannel(
+			channelId,
+			"Channel human readable title",
+			NotificationManager.IMPORTANCE_DEFAULT
+		)
+		notificationManager.createNotificationChannel(channel)
+	}
+
+	notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+}
 
 
 }
